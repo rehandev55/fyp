206e3640bd1452a84023746b705ab197e474deb7
MnzaJvd <mnzajaved@gmail.com>
2026-05-02 03:27:26 +0500

    practice section

--- resources/js/actions/App/Http/Controllers/Admin/ContentController.ts
+++ resources/js/actions/App/Http/Controllers/Admin/ContentController.ts
@@ -1,5 +1,4 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../../wayfinder'
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\Admin\ContentController::store
  * @see app/Http/Controllers/Admin/ContentController.php:18
