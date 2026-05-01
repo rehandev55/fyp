c50eb10bc16fc9a06010131aa6ea1f31d9a85538
MnzaJvd <mnzajaved@gmail.com>
2026-05-01 12:18:01 +0500

    .

--- resources/js/actions/App/Http/Controllers/AIControllers/ChatController.ts
+++ resources/js/actions/App/Http/Controllers/AIControllers/ChatController.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\AIControllers\ChatController::sessions
  * @see app/Http/Controllers/AIControllers/ChatController.php:18
--- resources/js/actions/Laravel/Fortify/Http/Controllers/VerifyEmailController.ts
+++ resources/js/actions/Laravel/Fortify/Http/Controllers/VerifyEmailController.ts
@@ -96,4 +96,10 @@ __invoke.head = (args: { id: string | number, hash: string | number } | [id: str
     __invoke.form = __invokeForm
 const VerifyEmailController = { __invoke }
 
+export default VerifyEmailController   method: 'get',
+        })
+    
+    __invoke.form = __invokeForm
+const VerifyEmailController = { __invoke }
+
 export default VerifyEmailController
