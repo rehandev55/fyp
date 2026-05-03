27bbc9cc6de507cfd6a99ea0a01b6a5c44a65e4b
MnzaJvd <mnzajaved@gmail.com>
2026-05-03 15:53:04 +0500

    .updatedSummary

--- resources/js/actions/App/Http/Controllers/Admin/UserController.ts
+++ resources/js/actions/App/Http/Controllers/Admin/UserController.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\Admin\UserController::index
  * @see app/Http/Controllers/Admin/UserController.php:16
--- resources/js/actions/App/Http/Controllers/ProgressController.ts
+++ resources/js/actions/App/Http/Controllers/ProgressController.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\ProgressController::__invoke
  * @see app/Http/Controllers/ProgressController.php:9
