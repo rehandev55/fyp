4279a85445d2d4aba4faa0a1005e08958c8adb4b
MnzaJvd <mnzajaved@gmail.com>
2026-05-04 16:45:19 +0500

    .

--- resources/js/routes/boost/index.ts
+++ resources/js/routes/boost/index.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
 /**
  * @see vendor/laravel/boost/src/BoostServiceProvider.php:91
  * @route '/_boost/browser-logs'
