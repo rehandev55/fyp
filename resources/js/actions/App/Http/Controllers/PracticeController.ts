682ff2a36f6aab49c47c78c7819e80465821e60c
AnumSheraz398 <168107636+AnumSheraz398@users.noreply.github.com>
2026-05-04 15:53:11 +0500

    retriever update

--- resources/js/actions/App/Http/Controllers/PracticeController.ts
+++ resources/js/actions/App/Http/Controllers/PracticeController.ts
@@ -1,5 +1,4 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../wayfinder'
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\PracticeController::__invoke
  * @see app/Http/Controllers/PracticeController.php:9
