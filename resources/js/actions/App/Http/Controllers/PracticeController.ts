9e5af40c5eb8c4ae9feea1bba285f7851353f20b
MnzaJvd <mnzajaved@gmail.com>
2026-05-04 17:07:23 +0500

    .

---
+++ resources/js/actions/App/Http/Controllers/PracticeController.ts
@@ -0,0 +1,80 @@
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../wayfinder'
+/**
+* @see \App\Http\Controllers\PracticeController::__invoke
+ * @see app/Http/Controllers/PracticeController.php:9
+ * @route '/practice'
+ */
+const PracticeController = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: PracticeController.url(options),
+    method: 'get',
+})
+
+PracticeController.definition = {
+    methods: ["get","head"],
+    url: '/practice',
+} satisfies RouteDefinition<["get","head"]>
+
+/**
+* @see \App\Http\Controllers\PracticeController::__invoke
+ * @see app/Http/Controllers/PracticeController.php:9
+ * @route '/practice'
+ */
+PracticeController.url = (options?: RouteQueryOptions) => {
+    return PracticeController.definition.url + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\PracticeController::__invoke
+ * @see app/Http/Controllers/PracticeController.php:9
+ * @route '/practice'
+ */
+PracticeController.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: PracticeController.url(options),
+    method: 'get',
+})
+/**
+* @see \App\Http\Controllers\PracticeController::__invoke
+ * @see app/Http/Controllers/PracticeController.php:9
+ * @route '/practice'
+ */
+PracticeController.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
+    url: PracticeController.url(options),
+    method: 'head',
+})
+
+    /**
+* @see \App\Http\Controllers\PracticeController::__invoke
+ * @see app/Http/Controllers/PracticeController.php:9
+ * @route '/practice'
+ */
+    const PracticeControllerForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+        action: PracticeController.url(options),
+        method: 'get',
+    })
+
+            /**
+* @see \App\Http\Controllers\PracticeController::__invoke
+ * @see app/Http/Controllers/PracticeController.php:9
+ * @route '/practice'
+ */
+        PracticeControllerForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+            action: PracticeController.url(options),
+            method: 'get',
+        })
+            /**
+* @see \App\Http\Controllers\PracticeController::__invoke
+ * @see app/Http/Controllers/PracticeController.php:9
+ * @route '/practice'
+ */
+        PracticeControllerForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+            action: PracticeController.url({
+                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                            _method: 'HEAD',
+                            ...(options?.query ?? options?.mergeQuery ?? {}),
+                        }
+                    }),
+            method: 'get',
+        })
+    
+    PracticeController.form = PracticeControllerForm
+export default PracticeController
--- resources/js/routes/boost/index.ts
+++ resources/js/routes/boost/index.ts
@@ -1,5 +1,4 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
 /**
  * @see vendor/laravel/boost/src/BoostServiceProvider.php:91
  * @route '/_boost/browser-logs'
