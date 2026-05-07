30e5e2710500a3b70e794d67f51843d386aadfe5
MnzaJvd <mnzajaved@gmail.com>
2026-05-07 18:18:41 +0500

    .

---
+++ resources/js/actions/App/Http/Controllers/Admin/UserController.ts
@@ -0,0 +1,415 @@
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../../wayfinder'
+/**
+* @see \App\Http\Controllers\Admin\UserController::index
+ * @see app/Http/Controllers/Admin/UserController.php:16
+ * @route '/api/users'
+ */
+export const index = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: index.url(options),
+    method: 'get',
+})
+
+index.definition = {
+    methods: ["get","head"],
+    url: '/api/users',
+} satisfies RouteDefinition<["get","head"]>
+
+/**
+* @see \App\Http\Controllers\Admin\UserController::index
+ * @see app/Http/Controllers/Admin/UserController.php:16
+ * @route '/api/users'
+ */
+index.url = (options?: RouteQueryOptions) => {
+    return index.definition.url + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\Admin\UserController::index
+ * @see app/Http/Controllers/Admin/UserController.php:16
+ * @route '/api/users'
+ */
+index.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: index.url(options),
+    method: 'get',
+})
+/**
+* @see \App\Http\Controllers\Admin\UserController::index
+ * @see app/Http/Controllers/Admin/UserController.php:16
+ * @route '/api/users'
+ */
+index.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
+    url: index.url(options),
+    method: 'head',
+})
+
+    /**
+* @see \App\Http\Controllers\Admin\UserController::index
+ * @see app/Http/Controllers/Admin/UserController.php:16
+ * @route '/api/users'
+ */
+    const indexForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+        action: index.url(options),
+        method: 'get',
+    })
+
+            /**
+* @see \App\Http\Controllers\Admin\UserController::index
+ * @see app/Http/Controllers/Admin/UserController.php:16
+ * @route '/api/users'
+ */
+        indexForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+            action: index.url(options),
+            method: 'get',
+        })
+            /**
+* @see \App\Http\Controllers\Admin\UserController::index
+ * @see app/Http/Controllers/Admin/UserController.php:16
+ * @route '/api/users'
+ */
+        indexForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+            action: index.url({
+                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                            _method: 'HEAD',
+                            ...(options?.query ?? options?.mergeQuery ?? {}),
+                        }
+                    }),
+            method: 'get',
+        })
+    
+    index.form = indexForm
+/**
+* @see \App\Http\Controllers\Admin\UserController::update
+ * @see app/Http/Controllers/Admin/UserController.php:31
+ * @route '/api/users/{id}'
+ */
+export const update = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'put'> => ({
+    url: update.url(args, options),
+    method: 'put',
+})
+
+update.definition = {
+    methods: ["put"],
+    url: '/api/users/{id}',
+} satisfies RouteDefinition<["put"]>
+
+/**
+* @see \App\Http\Controllers\Admin\UserController::update
+ * @see app/Http/Controllers/Admin/UserController.php:31
+ * @route '/api/users/{id}'
+ */
+update.url = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions) => {
+    if (typeof args === 'string' || typeof args === 'number') {
+        args = { id: args }
+    }
+
+    
+    if (Array.isArray(args)) {
+        args = {
+                    id: args[0],
+                }
+    }
+
+    args = applyUrlDefaults(args)
+
+    const parsedArgs = {
+                        id: args.id,
+                }
+
+    return update.definition.url
+            .replace('{id}', parsedArgs.id.toString())
+            .replace(/\/+$/, '') + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\Admin\UserController::update
+ * @see app/Http/Controllers/Admin/UserController.php:31
+ * @route '/api/users/{id}'
+ */
+update.put = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'put'> => ({
+    url: update.url(args, options),
+    method: 'put',
+})
+
+    /**
+* @see \App\Http\Controllers\Admin\UserController::update
+ * @see app/Http/Controllers/Admin/UserController.php:31
+ * @route '/api/users/{id}'
+ */
+    const updateForm = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+        action: update.url(args, {
+                    [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                        _method: 'PUT',
+                        ...(options?.query ?? options?.mergeQuery ?? {}),
+                    }
+                }),
+        method: 'post',
+    })
+
+            /**
+* @see \App\Http\Controllers\Admin\UserController::update
+ * @see app/Http/Controllers/Admin/UserController.php:31
+ * @route '/api/users/{id}'
+ */
+        updateForm.put = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+            action: update.url(args, {
+                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                            _method: 'PUT',
+                            ...(options?.query ?? options?.mergeQuery ?? {}),
+                        }
+                    }),
+            method: 'post',
+        })
+    
+    update.form = updateForm
+/**
+* @see \App\Http\Controllers\Admin\UserController::destroy
+ * @see app/Http/Controllers/Admin/UserController.php:23
+ * @route '/api/users/{id}'
+ */
+export const destroy = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
+    url: destroy.url(args, options),
+    method: 'delete',
+})
+
+destroy.definition = {
+    methods: ["delete"],
+    url: '/api/users/{id}',
+} satisfies RouteDefinition<["delete"]>
+
+/**
+* @see \App\Http\Controllers\Admin\UserController::destroy
+ * @see app/Http/Controllers/Admin/UserController.php:23
+ * @route '/api/users/{id}'
+ */
+destroy.url = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions) => {
+    if (typeof args === 'string' || typeof args === 'number') {
+        args = { id: args }
+    }
+
+    
+    if (Array.isArray(args)) {
+        args = {
+                    id: args[0],
+                }
+    }
+
+    args = applyUrlDefaults(args)
+
+    const parsedArgs = {
+                        id: args.id,
+                }
+
+    return destroy.definition.url
+            .replace('{id}', parsedArgs.id.toString())
+            .replace(/\/+$/, '') + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\Admin\UserController::destroy
+ * @see app/Http/Controllers/Admin/UserController.php:23
+ * @route '/api/users/{id}'
+ */
+destroy.delete = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
+    url: destroy.url(args, options),
+    method: 'delete',
+})
+
+    /**
+* @see \App\Http\Controllers\Admin\UserController::destroy
+ * @see app/Http/Controllers/Admin/UserController.php:23
+ * @route '/api/users/{id}'
+ */
+    const destroyForm = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+        action: destroy.url(args, {
+                    [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                        _method: 'DELETE',
+                        ...(options?.query ?? options?.mergeQuery ?? {}),
+                    }
+                }),
+        method: 'post',
+    })
+
+            /**
+* @see \App\Http\Controllers\Admin\UserController::destroy
+ * @see app/Http/Controllers/Admin/UserController.php:23
+ * @route '/api/users/{id}'
+ */
+        destroyForm.delete = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+            action: destroy.url(args, {
+                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                            _method: 'DELETE',
+                            ...(options?.query ?? options?.mergeQuery ?? {}),
+                        }
+                    }),
+            method: 'post',
+        })
+    
+    destroy.form = destroyForm
+/**
+* @see \App\Http\Controllers\Admin\UserController::toggleStatus
+ * @see app/Http/Controllers/Admin/UserController.php:58
+ * @route '/api/users/toggle/{id}'
+ */
+export const toggleStatus = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'patch'> => ({
+    url: toggleStatus.url(args, options),
+    method: 'patch',
+})
+
+toggleStatus.definition = {
+    methods: ["patch"],
+    url: '/api/users/toggle/{id}',
+} satisfies RouteDefinition<["patch"]>
+
+/**
+* @see \App\Http\Controllers\Admin\UserController::toggleStatus
+ * @see app/Http/Controllers/Admin/UserController.php:58
+ * @route '/api/users/toggle/{id}'
+ */
+toggleStatus.url = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions) => {
+    if (typeof args === 'string' || typeof args === 'number') {
+        args = { id: args }
+    }
+
+    
+    if (Array.isArray(args)) {
+        args = {
+                    id: args[0],
+                }
+    }
+
+    args = applyUrlDefaults(args)
+
+    const parsedArgs = {
+                        id: args.id,
+                }
+
+    return toggleStatus.definition.url
+            .replace('{id}', parsedArgs.id.toString())
+            .replace(/\/+$/, '') + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\Admin\UserController::toggleStatus
+ * @see app/Http/Controllers/Admin/UserController.php:58
+ * @route '/api/users/toggle/{id}'
+ */
+toggleStatus.patch = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'patch'> => ({
+    url: toggleStatus.url(args, options),
+    method: 'patch',
+})
+
+    /**
+* @see \App\Http\Controllers\Admin\UserController::toggleStatus
+ * @see app/Http/Controllers/Admin/UserController.php:58
+ * @route '/api/users/toggle/{id}'
+ */
+    const toggleStatusForm = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+        action: toggleStatus.url(args, {
+                    [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                        _method: 'PATCH',
+                        ...(options?.query ?? options?.mergeQuery ?? {}),
+                    }
+                }),
+        method: 'post',
+    })
+
+            /**
+* @see \App\Http\Controllers\Admin\UserController::toggleStatus
+ * @see app/Http/Controllers/Admin/UserController.php:58
+ * @route '/api/users/toggle/{id}'
+ */
+        toggleStatusForm.patch = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+            action: toggleStatus.url(args, {
+                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                            _method: 'PATCH',
+                            ...(options?.query ?? options?.mergeQuery ?? {}),
+                        }
+                    }),
+            method: 'post',
+        })
+    
+    toggleStatus.form = toggleStatusForm
+/**
+* @see \App\Http\Controllers\Admin\UserController::__invoke
+ * @see app/Http/Controllers/Admin/UserController.php:12
+ * @route '/admin/users'
+ */
+const UserController = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: UserController.url(options),
+    method: 'get',
+})
+
+UserController.definition = {
+    methods: ["get","head"],
+    url: '/admin/users',
+} satisfies RouteDefinition<["get","head"]>
+
+/**
+* @see \App\Http\Controllers\Admin\UserController::__invoke
+ * @see app/Http/Controllers/Admin/UserController.php:12
+ * @route '/admin/users'
+ */
+UserController.url = (options?: RouteQueryOptions) => {
+    return UserController.definition.url + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\Admin\UserController::__invoke
+ * @see app/Http/Controllers/Admin/UserController.php:12
+ * @route '/admin/users'
+ */
+UserController.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: UserController.url(options),
+    method: 'get',
+})
+/**
+* @see \App\Http\Controllers\Admin\UserController::__invoke
+ * @see app/Http/Controllers/Admin/UserController.php:12
+ * @route '/admin/users'
+ */
+UserController.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
+    url: UserController.url(options),
+    method: 'head',
+})
+
+    /**
+* @see \App\Http\Controllers\Admin\UserController::__invoke
+ * @see app/Http/Controllers/Admin/UserController.php:12
+ * @route '/admin/users'
+ */
+    const UserControllerForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+        action: UserController.url(options),
+        method: 'get',
+    })
+
+            /**
+* @see \App\Http\Controllers\Admin\UserController::__invoke
+ * @see app/Http/Controllers/Admin/UserController.php:12
+ * @route '/admin/users'
+ */
+        UserControllerForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+            action: UserController.url(options),
+            method: 'get',
+        })
+            /**
+* @see \App\Http\Controllers\Admin\UserController::__invoke
+ * @see app/Http/Controllers/Admin/UserController.php:12
+ * @route '/admin/users'
+ */
+        UserControllerForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+            action: UserController.url({
+                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                            _method: 'HEAD',
+                            ...(options?.query ?? options?.mergeQuery ?? {}),
+                        }
+                    }),
+            method: 'get',
+        })
+    
+    UserController.form = UserControllerForm
+UserController.index = index
+UserController.update = update
+UserController.destroy = destroy
+UserController.toggleStatus = toggleStatus
+
+export default UserController
